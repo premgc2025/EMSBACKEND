6e6753cb28ea95b95d255fd447a44dd863927705
premgc2025@gmail.com <premgc2025@gmail.com>
2024-10-11 12:02:10 +0545

    Allow access in cors seeting

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ import mongoose from 'mongoose'
 const app = express()
 
 
-app.use(cors({
-    origin:'http://localhost:5173',
-    credential: true
-}))
+app.use(cors())
+// app.use(cors({
+//     origin:'http://localhost:5173',
+//     credential: true
+// }))
 app.use(express.json())
 app.use(express.static('public/image'))
 
