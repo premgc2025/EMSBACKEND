7339a797f25f11e65a2776817990f0de82ac8218
premgc2025 <premgc2025@gmail.com>
2024-10-11 11:51:28 +0545

    Update Frontend site to allow cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://emsbackend-premgc.vercel.app/',
     credential: true
 }))
 app.use(express.json())
