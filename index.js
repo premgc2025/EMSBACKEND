78989e28c6fb4055bceb6536e10c654c991febe4
premgc2025@gmail.com <premgc2025@gmail.com>
2024-10-13 14:02:13 +0545

    CORS MODIFIED

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ import mongoose from 'mongoose'
 const app = express()
 
 
-// CORS configuration
-// const corsOptions = {
-//     origin: '*', // Allow your frontend origin
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
-//     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-//   };
+
   
-//   app.use(cors(corsOptions));
-app.use(cors())
+  app.use(cors(
+    {
+        origin: '*', // Allow your frontend origin
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+        allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+      }
+
+  ));
+
 app.use(express.json())
 app.use(express.static('public/image'))
 
